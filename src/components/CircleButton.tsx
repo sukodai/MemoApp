e0e4bca1cf06ee8b6804b789a7748c792f6ee530
sukodai <sukodai@gmail.com>
2024-06-13 14:58:02 +0900

    ADD components

---
+++ src/components/CircleButton.tsx
@@ -0,0 +1,48 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  ViewStyle,
+  TouchableOpacity,
+} from "react-native";
+
+interface Props {
+  children: JSX.Element;
+  style?: ViewStyle;
+  onPress?: () => void;
+}
+
+const CircleButton = (props: Props) => {
+  const { children, style, onPress } = props;
+  return (
+    <TouchableOpacity style={[styles.circleButton, style]} onPress={onPress}>
+      <Text style={styles.circleButtonLabel}>{children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: "#467fd3",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    shadowColor: "#000000",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: "#fff",
+    fontSize: 40,
+    lineHeight: 44,
+  },
+});
+
+export default CircleButton;
